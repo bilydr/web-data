83d050ea14ace77cea0ca17600201551e4878418
Longyi Bi <lbi@carlsonwagonlit.com>
2019-02-15 21:50:35 +0000

    add I-765 and minor refactoring

--- uscis.R
+++ uscis.R
@@ -6,6 +6,7 @@ library(lubridate)
 # NSC = Nebraska Service Center
 url_i140 <- "https://egov.uscis.gov/processing-times/api/processingtime/I-140/NSC"
 url_i485 <- "https://egov.uscis.gov/processing-times/api/processingtime/I-485/NSC"
+url_i765 <- "https://egov.uscis.gov/processing-times/api/processingtime/I-765/NSC"
 
 get_estimate <- function(url_api) {
   res <- fromJSON(url_api)
@@ -28,21 +29,46 @@ get_estimate <- function(url_api) {
     select(-range)
 }
 
-df1 <- get_estimate(url_i140)
-df2 <- get_estimate(url_i485)
+df1 <- get_estimate(url_i140) %>% add_column(form = "I-140")
+df2 <- get_estimate(url_i485) %>% add_column(form = "I-485")
+df3 <- get_estimate(url_i765) %>% add_column(form = "I-765")
 
-df <- bind_rows(df1, df2) %>%
-  mutate(Date_check = Sys.Date())
+df_new <- bind_rows(df1, df2, df3) %>%
+  mutate(Date_check = Sys.Date()) %>%
+  as_tibble()
 
+df_curr <- df_new %>%
+  filter(id %in% c("136A-E13", "I-485 Employment", "147-OTH")) %>%
+  select(form, form_type, date_pub, date_receipt, month_max, month_min)
 
-print(df[,1:3])
-
-file_db <- "uscis_proc_time.csv"
-
+# write entire data to database
+file_db <- "~/data/uscis_proc_time.csv"
+# append if existing data found
 if (file.exists(file_db)) {
-  df0 <- read_csv(file_db)
-  df <- bind_rows(df, df0) %>%
+  df_old <- suppressMessages(read_csv(file_db))
+  print(paste("Nb. records in DB Before update:", nrow(df_old)))
+  df_new <- bind_rows(df_new, df_old) %>%
     distinct()
 }
+print(paste("Nb. records in DB After update:", nrow(df_new)))
+write_csv(df_new, file_db)
 
-write_csv(df, file_db)
+# write latest data to current file
+file_current <- "~/data/uscis_current.csv"
+if (file.exists(file_current)) {
+  message("----Last results----")
+  df_curr_old <- suppressMessages(read_csv(file_current))
+  print(df_curr_old)
+}
+
+# if a data udpate is detected, save as a separate copy
+if (!setequal(df_curr_old, df_curr)) {
+  message("#### Update found !! ####")
+  date_pub_old <- df_curr_old$date_pub[1] %>% as.character()
+  file.rename(file_current, paste0("~/data/uscis", date_pub_old, ".csv"))
+}
+
+message("----Current results----")
+print(df_curr)
+write_csv(df_curr, file_current)
+
